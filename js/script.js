ae5a0d68def37404d5e33711fd4195bc1f20be89
ayaneey <ayan.h18@hotmail.com>
2022-08-01 12:40:22 +0100

    alt some changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 const changeColor = document.getElementById("changeColor"),
-  colors = ["blue", "green", "purple", "pink", "red", "orange", "yellow"];
+  colors = [
+    "#007bff",
+    "#6610f2",
+    "#e83e8c",
+    "#dc3545",
+    "#fd7e14",
+    "#ffc107",
+    "#28a745",
+    "#20c997",
+    "#17a2b8",
+    "#fff",
+  ];
 let colorIndex = 0;
 
 changeColor.addEventListener("click", () => {
   document.body.style.backgroundColor = colors[colorIndex];
   colorIndex = (colorIndex + 1) % colors.length;
 });
-
-??
